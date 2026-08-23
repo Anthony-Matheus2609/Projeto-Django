6556691a1bfbdee57ada6abda409601e22cfc66d
Anthony-Matheus2609 <maamalmeida@gmail.com>
2026-08-23 21:49:48 +0000

    MExi um pouco de url aq

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -27,7 +27,8 @@ urlpatterns = [
     path("", core_views.index),
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
-    path('dashboard/', views.painel_dashboard, name = 'dashboard')
+    path('dashboard/', views.painel_dashboard),
+    path('detalhe_equipamento/', views.detalhe_equipamento),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
